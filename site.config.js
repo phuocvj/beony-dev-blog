48bf41b891e19e813f7cc117cc89a443a171e46d
Lê Thiện Phước <lethienphuoc@outlook.com>
2024-07-11 09:12:59 +0700

    Fix some bugs

--- site.config.js
+++ site.config.js
@@ -12,12 +12,12 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `beony-dev-blog`,
-      href: "https://github.com/phuocvj/morethan-log",
-    },
-    {
       name: `VJmyID`,
       href: "https://vjid.changshin.vn",
+    },
+    {
+      name: `ChangShin Community`,
+      href: "https://community.changshin.vn",
     },
   ],
   // blog setting (required)
@@ -28,7 +28,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://peonydevblog.vercel.app",
+  link: "https://beonyblog.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
